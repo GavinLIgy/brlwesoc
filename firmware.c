c02c594b230cd2a8fe90210542b1394c89ee50a9
GavinLI <36605947+GavinLIgy@users.noreply.github.com>
2020-02-07 21:29:06 +0800

    Update firmware.c

--- firmware.c
+++ firmware.c
@@ -887,8 +887,8 @@ void main()
 
 	//test: RNG generation
 	print("\nRNG generation:\r\n");
-	debug_rdcycle();
-	setseed32(test_1);
+	//debug_rdcycle();
+	//setseed32(test_1);
 	debug_rdcycle();
 	getrandom(test_5);
 	BRLWE_init_hex(&m, test_5, 0);

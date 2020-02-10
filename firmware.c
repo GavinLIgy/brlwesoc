dd4369834939effb69c32f1dd12f402fbc9135ba
GavinLI <36605947+GavinLIgy@users.noreply.github.com>
2020-02-10 17:14:45 +0800

    Update firmware.c

--- firmware.c
+++ firmware.c
@@ -310,7 +310,7 @@ void print_dec(uint32_t v)
 		print(">=10Billion");
 		return;
 	}
-	/*
+	
 	if (v >= 9000000000) { putchar('9'); v -= 9000000000; }
 	else if (v >= 8000000000) { putchar('8'); v -= 8000000000; }
 	else if (v >= 7000000000) { putchar('7'); v -= 7000000000; }
@@ -330,7 +330,7 @@ void print_dec(uint32_t v)
 	else if (v >= 300000000) { putchar('3'); v -= 300000000; }
 	else if (v >= 200000000) { putchar('2'); v -= 200000000; }
 	else if (v >= 100000000) { putchar('1'); v -= 100000000; }
-	*/
+	
 	if (v >= 90000000) { putchar('9'); v -= 90000000; }
 	else if (v >= 80000000) { putchar('8'); v -= 80000000; }
 	else if (v >= 70000000) { putchar('7'); v -= 70000000; }
@@ -648,7 +648,7 @@ void alloc_test(void)
     }
     mem_print();
     
-    for(i=0;i<4;i++)
+    /*for(i=0;i<4;i++)
     {
         for(j=0;j<4;j++)
         {
@@ -657,7 +657,7 @@ void alloc_test(void)
     }
     m_free(ptr);
     mem_print();
-    buff_print((unsigned char *)ptr, 16);
+    buff_print((unsigned char *)ptr, 16);*/
 }
 
 /*------------rng component------------------*/

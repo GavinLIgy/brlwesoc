f5e6144f485a30a3783a084fd3a33efa75542942
GavinLI <36605947+GavinLIgy@users.noreply.github.com>
2020-02-22 22:12:19 +0800

    Update user_ram.v

--- user_ram.v
+++ user_ram.v
@@ -10,16 +10,17 @@ module user_ram#(
 	output [31:0]           do_o,
 );
 
-    reg [31:0]          bram[2^(ADDR_BIT) - 1:0];    
+    reg [31:0]       bram[2^(ADDR_BIT) - 1:0];    
     integer          i;   
     reg [31:0]       data;
 
-    always @(posedge clk_i or posedge rst_i)
+    always @(posedge clk_i)
     begin
        if (!rst_i)   
          begin
-           for(i = 0 ; i <= (2^(ADDR_BIT) - 1); i = i+1) //reset by bit
-           bram[i] <= 32'b0;
+			data <= 32'b0;
+			for(i = 0 ; i <= (2^(ADDR_BIT) - 1); i = i+1) //reset by word
+			bram[i] <= 32'b0;
          end
        else if (wr_en_i) begin
             bram[addr_i] <= di_i;
@@ -28,9 +29,9 @@ module user_ram#(
             data <= bram[addr_i];
        end
        else begin
-        data <= 32'b1;      //读写均无效时，为1
+			data <= 32'b0;      //读写均无效时，为1
        end
     end
 
-    assign do_o = rd_en_i? data : 32'b1;
+    assign do_o = rd_en_i? data : 32'b0;
 endmodule
